e9f58d9fceade11e86a06ccd030356db2f6caa13
aseel-gha <aseel.gh@anvilerp.com>
2024-03-10 15:25:14 +0200

    monthly_variables

--- bounya/albounya/doctype/monthly_variables/monthly_variables.js
+++ bounya/albounya/doctype/monthly_variables/monthly_variables.js
@@ -1,8 +1,59 @@
 // Copyright (c) 2024, ARD Company and contributors
 // For license information, please see license.txt
+let employee_list = [];
 
 frappe.ui.form.on('Monthly Variables', {
-	// refresh: function(frm) {
+	
+	refresh: function (frm) {
+		const today = new Date();
+		let month = today.getMonth(); // returns from 0 to 11
+		frm.doc.month = month+1;
+		frm.refresh_fields();
 
-	// }
+	},
+
+	onload: function(frm){
+		// get Salary Components from payroll settings.
+		frappe.call({
+			method :"get_salary_components",
+			doc:frm.doc,
+			callback:function(r){
+				if(r.message){
+					// console.log("aaaaaaaaaaaaaaaaaaaaaaaaa");
+					// console.log(r.message);
+					employee_list = r.message;
+					console.log(employee_list);
+				}
+			}
+		});
+		
+		frm.set_query("salary_component", function () {
+			return {
+			  filters: [
+				["Salary Component", 'salary_component', 'in',  employee_list ],
+			  ],
+			};
+		  });
+
+	  
+	},
+
+	month(frm) {
+		if (frm.doc.month) {
+
+			const d = new Date();
+			let year = d.getFullYear();
+			// console.log(`${year}`);
+
+			let month = frm.doc.month
+			var dateString = '' + year + '-' + month + '-' + '25';
+			var combined = new Date(dateString);
+			
+			frm.doc.to = new Date(combined);
+			let from_date =  frappe.datetime.add_months(combined, -1);
+			frm.doc.from = new Date(from_date);
+			frm.refresh_fields();
+		}
+	},
+
 });
--- bounya/albounya/doctype/monthly_variables/monthly_variables.py
+++ bounya/albounya/doctype/monthly_variables/monthly_variables.py
@@ -1,8 +1,29 @@
 # Copyright (c) 2024, ARD Company and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MonthlyVariables(Document):
-	pass
+	
+	def validate(self):
+		self.validate_employess()
+	
+	def on_submit(self):
+		self.create_additional_salary()
+	
+	@frappe.whitelist()
+	def get_salary_components(self):
+		components = []
+		component_list = frappe.db.get_list('Salary Component Settings', fields=['salary_component'],)
+
+		for c in component_list:
+			components.append(str(c.salary_component))
+		# frappe.msgprint(str(components))
+		return components
+	
+	def validate_employess(self):
+		pass
+
+	def create_additional_salary(self):
+		pass
