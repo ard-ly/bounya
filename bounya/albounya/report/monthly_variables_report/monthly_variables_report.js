5d1537fb45d7b346023dc0ef2efeb7b4a9e4d4b7
aseel-gha <aseel.gh@anvilerp.com>
2024-04-01 11:43:47 +0200

    monthly_variables_report

--- bounya/albounya/report/monthly_variables_report/monthly_variables_report.js
+++ bounya/albounya/report/monthly_variables_report/monthly_variables_report.js
@@ -2,8 +2,52 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
+// const d = new Date();
+// var cur_year = d.getFullYear();
+// var pre_month = d.getMonth() ;
+// cur_month = pre_month +1
+// var to_d = '' + cur_year + '-' + cur_month + '-' + '25';
+// var from_d = '' + cur_year + '-' + pre_month + '-' + '25';
+// console.log(new Date(from_d));
+// console.log(new Date(to_d));
+
 frappe.query_reports["Monthly Variables Report"] = {
 	"filters": [
 
+		{
+			label: __("Month"),
+			fieldname: "month",
+			fieldtype: "Select",
+			options: "\nJanuary\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember",
+		   },
+		   {
+			label: __("From"),
+			fieldname: "from_date",
+			fieldtype: "Date",
+		   },
+		   {
+			label: __("To"),
+			fieldname: "to_date",
+			fieldtype: "Date",
+		   },
+		   {
+			label: __("Salary Component"),
+			fieldname: "salary_component",
+			fieldtype: "Link",
+			options: "Salary Component",
+		   },
+		   {
+			label: __("Salary Component Type"),
+			fieldname: "salary_component_type",
+			fieldtype: "Select",
+			options: "\nEarning\nDeduction",
+		   },
+		   {
+			label: __("Employee"),
+			fieldname: "employee",
+			fieldtype: "Link",
+			options: "Employee",
+		   },
+		  
 	]
 };
