0ae2b9105a2537e65530dbba89152e6187041a04
aseel-gha <aseel.gh@anvilerp.com>
2024-06-25 15:09:26 +0400

    external_loans Report

--- bounya/albounya/report/employee_external_loans/employee_external_loans.js
+++ bounya/albounya/report/employee_external_loans/employee_external_loans.js
@@ -4,6 +4,36 @@
 
 frappe.query_reports["Employee External Loans"] = {
 	"filters": [
+		{
+			label: __("Employee"),
+			fieldname: "employee",
+			fieldtype: "Link",
+			options: "Employee",
+		},
+		{
+			label: __("Department"),
+			fieldname: "department",
+			fieldtype: "Link",
+			options: "Department",
+		},
+		{
+			label: __("Status"),
+			fieldname: "status",
+			fieldtype: "Select",
+			options: [
+				{ "value": "Draft", "label": __("Draft") },
+				{ "value": "Paid", "label": __("Paid") },
+				{ "value": "Unpaid", "label": __("Unpaid") },
+				{ "value": "Cancelled", "label": __("Cancelled") },
+			],
+			// default: "Unpaid",	
+		},
+		{
+			label: __("Type"),
+			fieldname: "type",
+			fieldtype: "Link",
+			options: "External Loans Type",
+		},
 
 	]
 };
