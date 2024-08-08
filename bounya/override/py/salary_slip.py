a379f078554f14f42a32443311bec14124c8c1f4
ardco <github@ard.ly>
2024-08-08 10:39:36 +0200

    تعديل مؤقت

--- bounya/override/py/salary_slip.py
+++ bounya/override/py/salary_slip.py
@@ -125,5 +125,5 @@ class CustomSalarySlip(SalarySlip):
     #     frappe.db.set_value("Salary Slip" , self.name , "docstatus" , 1 )
 
 
-    def validate(self):
-        pass
+    # def validate(self):
+    #     pass
