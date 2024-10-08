4b20ead13bea128a28956a680ce9079b178bc6bb
aseel-gha <aseel.gh@anvilerp.com>
2024-10-08 16:42:52 +0400

    8

--- bounya/albounya/doctype/buildings/buildings.js
+++ bounya/albounya/doctype/buildings/buildings.js
@@ -25,6 +25,40 @@ frappe.ui.form.on('Buildings', {
 		});
 	},
 	refresh(frm) {
+
+		if (frm.doc.latitude && frm.doc.longitude) {
+            frm.doc.geolocation = `{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[${frm.doc.longitude},${frm.doc.latitude}]}}]}`;
+            frm.refresh_fields();
+            // Ensure the map is available and initialized
+            if (frm.fields_dict.geolocation.map) {
+                let map = frm.fields_dict.geolocation.map;
+        
+                if (map) {
+                    // Remove the previous marker if it exists
+                    if (frm.marker) {
+                        map.removeLayer(frm.marker);
+                    }
+        
+                    // Add a new marker at the current location (make sure the order is [longitude, latitude])
+                    frm.marker = L.marker([frm.doc.latitude, frm.doc.longitude]).addTo(map);
+        
+                    // Optionally, bind a popup to the marker
+                    frm.marker.bindPopup("Your location").openPopup();
+        
+                    // Center the map on the new marker
+                    map.setView([frm.doc.latitude, frm.doc.longitude], 13);
+                }
+            }
+    
+            // Correct the order of latitude and longitude in GeoJSON
+        }
+        frm.refresh_fields();
+        console.log(frm.doc.geolocation);
+
+
+
+
+
 		// Clear the existing breadcrumbs. Set custom breadcrumbs will not do this automatically
 				frappe.breadcrumbs.clear();
 				
