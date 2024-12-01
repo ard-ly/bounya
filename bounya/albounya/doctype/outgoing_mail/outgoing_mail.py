3f267fb317ec036b8ca20880a8f81dca3aca1a58
omarjaber <omar.ja93@gmail.com>
2024-12-01 14:12:22 +0200

    set new autoname function

--- bounya/albounya/doctype/outgoing_mail/outgoing_mail.py
+++ bounya/albounya/doctype/outgoing_mail/outgoing_mail.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2024, ARD Company and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class OutgoingMail(Document):
-	pass
+    def autoname(self):
+        year = self.message_registration_date[:4]
+        short_year = year[-2:]
+        
+        count = frappe.db.count(
+            'Outgoing Mail',
+            filters={
+                'name': ['like', f"{short_year}-%"]
+            }
+        )
+        
+        sequence_number = count + 1
+        
+        formatted_sequence = f"{sequence_number:04d}"
+        
+        self.name = f"{short_year}-{formatted_sequence}"
+
