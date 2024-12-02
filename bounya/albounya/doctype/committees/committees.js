b8b4fd2e111088a30191f0e2ba3295ad333321ac
omarjaber <omar.ja93@gmail.com>
2024-12-02 13:32:43 +0200

    Create committees doctype

---
+++ bounya/albounya/doctype/committees/committees.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ARD Company and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Committees', {
+	// refresh: function(frm) {
+
+	// }
+});
