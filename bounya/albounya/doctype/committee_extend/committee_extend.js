772d67d0b070c97e8ddca06b87aefda44db75fb9
omarjaber <omar.ja93@gmail.com>
2024-12-02 15:00:47 +0200

    update committee extend and link with committee

--- bounya/albounya/doctype/committee_extend/committee_extend.js
+++ bounya/albounya/doctype/committee_extend/committee_extend.js
@@ -2,7 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Committee Extend', {
-	// refresh: function(frm) {
+	onload(frm) {
+        frm.set_query("committee", function() {
+            return {
+                filters: [
+                    ["Committees","docstatus", "=", 1]
+                ]
+            }
+        });
+    },
+    validate(frm) {
+		frm.events.validate_committee_dates(frm);
+    },
+    committee_extend_from(frm) {
+		frm.events.validate_committee_dates(frm);
+    },
+    committee_extend_to(frm) {
+		frm.events.validate_committee_dates(frm);
+    },
+    validate_committee_dates(frm) {
+        let committee_extend_from = frm.doc.committee_extend_from;
+        let committee_extend_to = frm.doc.committee_extend_to;
 
-	// }
+        if (committee_extend_from && committee_extend_to && committee_extend_from > committee_extend_to) {
+            frappe.msgprint(__('Committee From date must be less than or equal to Committee To date.'));
+            
+            frappe.validated = false;
+        }
+    }
 });
--- bounya/albounya/doctype/committee_extend/committee_extend.py
+++ bounya/albounya/doctype/committee_extend/committee_extend.py
@@ -1,8 +1,73 @@
-# Copyright (c) 2024, ARD Company and contributors
+# Copyright (c) 2024, Omar Jaber and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
+
 class CommitteeExtend(Document):
-	pass
+    def on_submit(self):
+        self.update_committee_dates()
+        # self.send_extend_notification()
+
+
+    @frappe.whitelist()
+    def update_committee_dates(self):
+        frappe.db.sql("update `tabCommittees` set committee_from='{0}', committee_to='{1}' where name='{2}'".format(self.committee_extend_from, self.committee_extend_to, self.committee))
+        frappe.db.commit()
+        frappe.msgprint(_('Document updated successfully'), alert=True, indicator='green')
+        
+
+    @frappe.whitelist()
+    def send_extend_notification(self):
+        blocked_users = ['Administrator']
+        hr_managers = frappe.get_all('Has Role', filters={'role': 'HR Manager', 'parenttype': 'User'}, fields=['parent'])
+        
+        hr_manager_emails = [
+            manager['parent'] 
+            for manager in hr_managers 
+            if manager['parent'] not in blocked_users
+        ]
+        
+        if hr_manager_emails:
+
+            link = frappe.utils.get_url_to_form('Committees', self.committee)
+
+            message = f"""
+            <div style='direction: rtl; text-align: right;'>
+                تم تمديد اللجنة <a href='{link}'>{self.committee}</a>:<br><br>
+                <table border="1" class="text-center" style="border-collapse: collapse; width: 70%;">
+                    <thead>
+                        <tr>
+                            <th width="20%"></th>
+                            <th width="40%">من تاريخ</th>
+                            <th width="40%">حتى تاريخ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>السابق</th>
+                            <td>{self.committee_from}</td>
+                            <td>{self.committee_to}</td>
+                        </tr>
+                        <tr>
+                            <th>الحالي</th>
+                            <td>{self.committee_extend_from}</td>
+                            <td>{self.committee_extend_to}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            """
+
+            subject = f"تمديد لجنة: {self.committee}"
+            frappe.sendmail(
+                recipients=hr_manager_emails,
+                subject=subject,
+                message=message,
+                reference_doctype=self.doctype,
+                reference_name=self.committee,
+                delayed=False
+            )
+
