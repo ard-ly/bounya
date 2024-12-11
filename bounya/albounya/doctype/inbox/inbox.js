2d9d3a42114d8e5824b65a08812e42ebee8ddf79
omarjaber <omar.ja93@gmail.com>
2024-12-11 11:09:56 +0200

    update inbox doctype

--- bounya/albounya/doctype/inbox/inbox.js
+++ bounya/albounya/doctype/inbox/inbox.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inbox', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm) {
+		frm.set_query('mail_type', function () {
+            return {
+                filters: [
+                    ['name', 'in', ['Incoming Mail', 'Outgoing Mail']]
+                ]
+            };
+        });
+    },
 });
