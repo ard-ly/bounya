03ad9eda918b0526f3f278b96cd1109da75964ee
omarjaber <omar.ja93@gmail.com>
2025-01-13 13:40:54 +0200

    add marginalize child table

--- bounya/albounya/doctype/incoming_mail/incoming_mail.js
+++ bounya/albounya/doctype/incoming_mail/incoming_mail.js
@@ -2,6 +2,41 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Incoming Mail', {
+    validate: function (frm) {
+        (frm.doc.marginalize || []).forEach(row => {
+            if (!row.employee && row.owner) {
+                frappe.call({
+                    method: 'frappe.client.get_value',
+                    args: {
+                        doctype: 'Employee',
+                        filters: { user_id: row.owner },
+                        fieldname: 'name'
+                    },
+                    async: false,
+                    callback: function (response) {
+                        if (response.message && response.message.name) {
+                            row.employee = response.message.name;
+
+                            frappe.call({
+                                method: 'frappe.client.get_value',
+                                args: {
+                                    doctype: 'Department',
+                                    filters: { custom_department_manager: row.employee },
+                                    fieldname: 'name'
+                                },
+                                async: false,
+                                callback: function (dept_response) {
+                                    if (dept_response.message) {
+                                        row.department = dept_response.message.name;
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    },
 	refresh: function(frm) {
 		if (frm.doc.docstatus === 1) {
             frm.page.sidebar.find('.attachments-actions').hide(); // Hide the "Add Attachment" button
@@ -68,3 +103,44 @@ frappe.ui.form.on('Incoming Mail', {
 		frm.set_value("outgoing_message_subject", )
 	}
 });
+
+
+
+// frappe.ui.form.on('Marginalize User', {
+//     marginalize: function (frm, cdt, cdn) {
+//         let row = frappe.get_doc(cdt, cdn);
+
+//         if (!row.employee) {
+//             frappe.call({
+//                 method: 'frappe.client.get_value',
+//                 args: {
+//                     doctype: 'Employee',
+//                     filters: { user_id: frappe.session.user },
+//                     fieldname: 'name'
+//                 },
+//                 callback: function (response) {
+//                     if (response.message) {
+//                         frappe.model.set_value(cdt, cdn, 'employee', response.message.name);
+
+//                         frappe.call({
+//                             method: 'frappe.client.get_value',
+//                             args: {
+//                                 doctype: 'Department',
+//                                 filters: { custom_department_manager: response.message.name },
+//                                 fieldname: 'name'
+//                             },
+//                             callback: function (dept_response) {
+//                                 if (dept_response.message) {
+//                                     frappe.model.set_value(cdt, cdn, 'department', dept_response.message.name);
+//                                 }
+//                             }
+//                         });
+
+//                     }
+//                 }
+//             });
+//         }
+
+//     }
+// });
+
---
+++ bounya/albounya/doctype/marginalize_user/marginalize_user.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, ARD Company and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class MarginalizeUser(Document):
+	pass
